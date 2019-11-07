16f7baf1efeaddef03f8729acd36d13d7562adb2
arthurgonze <arthurgonze@yahoo.com.br>
2019-11-07 00:25:50 -0300

    Base

---
+++ CodenamePinball/Assets/Sandbox/Arthur/Scripts/Block.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //config params
+    [SerializeField]
+    int maxHits = 3;
+
+    // cached reference
+    Level level;
+    GameManager gameManager;
+
+    // state variables
+    [SerializeField] int timesHit = 0; //only serialized for debug purposes
+
+    private void Start()
+    {
+        CountBlocks();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void CountBlocks()
+    {
+        level = FindObjectOfType<Level>();
+
+        if (tag == "Breakable")
+        {
+            level.addOneBreakableBlock();
+        }
+        else if (tag == "Unbreakable")
+        {
+            level.addOneUnbreakableBlock();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        Destroy(gameObject);//destroi o objeto
+        gameManager.AddToScore(); // aumenta a pontuacao do jogador
+        level.removeOneBreakableBlock(); // diminui em 1 o numero total de blocos da fase
+    }
+}
---
+++ CodenamePinball/Assets/Sandbox/Arthur/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    //config params
+    [Range(0f,1f)] [SerializeField] 
+    private float gameSpeed = 1f;
+    
+    [SerializeField] 
+    private int pointsPerBlockDestroyed = 42;
+    
+    [SerializeField] 
+    private TextMeshProUGUI scoreText;
+    
+    [SerializeField] 
+    private bool isAutoPlayEnabled = false;
+
+    // state variables
+    [SerializeField] 
+    private int P1Score = 0;
+
+    [SerializeField] 
+    private int P2Score = 0;
+
+    private void Awake()
+    {
+        // Singleton
+        int gameManagerCount = FindObjectsOfType<GameManager>().Length;
+        if(gameManagerCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        scoreText.SetText(P1Score.ToString());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        P1Score += pointsPerBlockDestroyed;
+        scoreText.SetText(P1Score.ToString());
+    }
+
+    public void Reset()
+    {
+        Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+}
